351ed817c1b4503445c284d0c29d96865ec8d90a
Isaac Cohen <isaaclandoncohen@gmail.com>
2014-07-16 12:27:58 -0700

    adding load gif

--- js/addSocialMedia.js
+++ js/addSocialMedia.js
@@ -1,12 +1,10 @@
   
 
       var SOCIAL_MEDIA = [
-       // ["Download" , "download_1.png" , "audio/needs.zip"],
         ["TWITTER" ,"twitter_1.png" , "http://twitter.com/share?text=New%20interactive%20EP%20from%20@JJ_VERNE%20coded%20by%20@cabbibo%20&url=http://cabbibo.github.io/needs"],
         ["FACEBOOK", "facebook_1.png" , 'http://www.facebook.com/sharer.php?u=http://cabbibo.github.io/needs'],
         ["SOUNDCLOUD" ,"soundcloud_1.png" , "https://soundcloud.com/jj-verne"],
         ["CABBIBO" , "cabbibo_1.png" , "http://cabbibo.com"],
-       // ["Information" , "info_1.png" , "" ],
       ]
       
 
